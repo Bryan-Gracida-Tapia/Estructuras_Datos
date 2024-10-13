603dcf33a68a8192efc1e8f208fcdc65f81208c1
Bryan_SC2 <bryangtapia@gmail.com>
2024-10-13 17:19:02 -0600

    En este archivo se emplean los diferentes tipos de datos en Python.

---
+++ Fundamentos_python/Tipos_de_Datos_Ejercicio.py
@@ -0,0 +1,36 @@
+# Gracida Tapia Bryan
+# 13 de octubre del 2024
+# Descripcion:
+# En el siguiente archivo pondremos en practica lo visto de tipos de datos y su declaracion
+
+# Creamos todas nuestras variables y les asignamos el tipo de dato requerido.
+dia = 'Lunes'       # Usamos una variable de tipo String
+pasaje = 20     # Usamos una variable de tipo Int
+comida = 135.5      # Usamos una variable de tipo Float
+otros_gastos = 34.5     # Usamos una variable de tipo Float
+variable_presupuesto = True    # Usamos una variable de tipo Booleana
+
+# Se imprimen todas las variables con informacion adicional para mejor comprencion
+print(" ***  Gastos Diarios  ***")
+print("Dia:", dia)
+print("Pasaje:", pasaje)
+print("Comida: ", comida, "$.")
+print("Otros gastos: ", otros_gastos, "$.")
+print("Fue mayor a mi presupuesto?: ", variable_presupuesto)
+
+# Se modifican las variables.
+dia = 'Martes'
+pasaje = 12
+comida = 45.5
+otros_gastos = 4.5
+variable_presupuesto = False
+
+print()     # hacemos un salto  de linea para separar los gastos
+
+# Se vuelven a imprimir todas las variables con los tipos de datos modificados
+print(" ***  Gastos Diarios  ***")
+print("Dia:", dia)
+print("Pasaje:", pasaje)
+print("Comida: ", comida, "$.")
+print("Otros gastos: ", otros_gastos, "$.")
+print("Fue mayor a mi presupuesto?: ", variable_presupuesto)
