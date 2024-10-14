f4519068fb8ad6b8e446f88ac3e486e5661967a5
Bryan Gracida Tapia <bryangtapia@gmail.com>
2024-10-14 16:31:09 -0600

    En el siguiente archivo se pone en practica las entradas de consola(input) en Python.

---
+++ Fundamentos_python/Entrada_consola_ejercicio.py
@@ -0,0 +1,34 @@
+# Gracida Tapia Bryan.
+# 14 de octubre del 2024.
+# Descripción:
+# Ejemplificación de entrada por consolas y operaciones basicas.
+
+
+# Declaramos nuestras variables.
+primer_numero = input("Introduce un número decimal: ")      # Les agregamos un input para asi generar una entrada de datos por consola.
+segundo_numero = input("Introduce otro número decimal: ")
+# Convertimos los valores recibidos anteriormente en flotantes.
+primer_float = float(primer_numero)
+segundo_float = float(segundo_numero)
+# Operaciones basicas.
+
+resultado_float = primer_float + segundo_float # Operación de suma.
+print()
+print(" ****  Suma  ****")
+print(f"El resultado de la suma de {primer_float} y {segundo_float} es: {resultado_float}")
+
+resultado_float = primer_float - segundo_float # Operación de resta.
+print()
+print(" ****  Resta  ****")
+print(f"El resultado de la resta de {primer_float} y {segundo_float} es: {resultado_float}")
+
+resultado_float = primer_float * segundo_float # Operación de multiplicación.
+print()
+print(" ****  Multiplicacion  ****")
+print(f"El resultado de la multiplicación de {primer_float} y {segundo_float} es: {resultado_float}")
+
+resultado_float = primer_float / segundo_float # Operación de divición.
+print()
+print(" ****  División  ****")
+print(f"El resultado de la división de {primer_float} y {segundo_float} es: {resultado_float}")
+
