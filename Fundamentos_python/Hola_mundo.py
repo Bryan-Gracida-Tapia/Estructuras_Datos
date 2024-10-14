5ddac424f3460313dddc6a88dd73dc8bd981eb19
Bryan_SC2 <bryangtapia@gmail.com>
2024-10-13 23:51:14 -0600

    En este archivo se ejemplifica el uso de los letreros en Python.

--- Fundamentos_python/Hola_mundo.py
+++ Fundamentos_python/Hola_mundo.py
@@ -1,6 +1,6 @@
 # Gracida Tapia Bryan
 # 13 de octubre de 2024
-# Programa_de_inicializacion_al_curso
+# Programa de inicialización al curso
 # En este archivo se ejemplifica el uso de letreros en Python.
 
-print("Hola mundo")         # la funcion print se utiliza para motrar letreros en pantalla
+print("Hola mundo")         # la función print se utiliza para motrar letreros en pantalla
