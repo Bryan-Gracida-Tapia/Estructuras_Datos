ac8607b12c92694e6d78463a7dbf801c7115d54c
Bryan Gracida Tapia <bryangtapia@gmail.com>
2024-10-16 15:53:11 -0600

    En el archivo ejemplificaremos los operadores de asignación racionales en Python.

---
+++ Operadores_python/Operadores_logicos.py
@@ -0,0 +1,8 @@
+# Gracida Tapia Bryan.
+# 16 de octubre del 2024.
+# Descripción:
+# Ejemplificacion de operadores logicos en Python.
+
+
+#/////////////////////////////////////////////////////////////////////////////////////////
+# Primer ejercicio
