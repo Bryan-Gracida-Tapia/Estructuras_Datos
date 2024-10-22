c1db3834eb2b675cc76998e821c6a72f59710632
Bryan Gracida Tapia <bryangtapia@gmail.com>
2024-10-22 09:18:19 -0600

    En este archivo se pone en prectica lo visto sobre conversiones en Python. (10)

--- Fundamentos_python/Entrada_conversiones_ejercicio.py
+++ Fundamentos_python/Entrada_conversiones_ejercicio.py
@@ -4,20 +4,18 @@
 # Entrada de datos por consola para interacturar con el usuario con valores dinámicos.
 
 #/////////////////////////////////////////////////////////////////////////////////////////
-# Declaramos nuestras variables con las lecturas por consola .
+# Declaramos nuestras variables con las lecturas por consola asu vez.
+# A su vez realizamos las conversiones correspondientes mediante la iteracción por consola que se realiza anteriormente.
 nombre_profesor = input("ingrese su nombre: ")
-numero_cubo = input("ingrese su número de cubículo: ")
-horas_de_clase_semanal = input("ingrese el número den horas de clases que imparte a la semana: ")
+numero_cubo = int(input("ingrese su número de cubículo: "))
+horas_de_clase_semanal = float(input("ingrese el número den horas de clases que imparte a la semana: "))
 años = input("Lleva más de cinco años en la unsij? si/no>  ")
 
-# Realizamos los castings correspondientes para tener datos mas fáciles de manipular.
-nombre_profesor= str(nombre_profesor)
-numero_cubo= int(numero_cubo)
-horas_de_clase_semanal= float(horas_de_clase_semanal)
+# En la variable años se utiliza la palabra reservada lower para convertir a minúsculas, y posteriormente comparar con la cadena "si".
 años= años.lower()=='si'
 
 # Imprimimos nuestras variables con información adicional para mejor comprención de cada letrero.
 print(f"Su nombre es: {nombre_profesor}")
 print(f"Su número de cubículo es: {numero_cubo}")
-print(f"Horas impartidas a la semanana : {horas_de_clase_semanal:.3f}")
+print(f"Horas impartidas a la semanana : {horas_de_clase_semanal:.1f}")
 print(f"Tiene más de cinco años en la UNSIJ?: {años}")
