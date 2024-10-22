97138122df056a7dc214076e6a4e13b911b3c39e
Bryan Gracida Tapia <bryangtapia@gmail.com>
2024-10-21 21:56:32 -0600

    En el archivo ejemplificaremos los operadores de basicos en Python. (7)

--- Fundamentos_python/Entrada_consola.py
+++ Fundamentos_python/Entrada_consola.py
@@ -4,18 +4,18 @@
 # Entrada de datos por consola para interacturar con el usuario con valores dinámicos.
 
 
-# Comentar sobre la función input.
+# En Python utilizamos la palabra reservada input para permitir la entrada de datos por consola, asignandolo a su vez a una variable.
 numero1_cadena = input("Introduce un número decimal: ")
 numero2_cadena = input("Introduce otro número decimal: ")
-resultado_cadena = numero1_cadena + numero2_cadena # Verificar qué es lo que realiza esta instrucción (ver el print).
+resultado_cadena = numero1_cadena + numero2_cadena # Asumiendo que el usuario ingreso dos numeros sin algun caracter la funcion deberia ser la suma de los dos numeros proporcionados.
 print()
 print(" ****  Recibir número sin un casting de varibles  ****")
-print(f"El resultado de {numero1_cadena} y {numero2_cadena} es: {resultado_cadena}")
+print(f"El resultado de {numero1_cadena} y {numero2_cadena} es: {resultado_cadena}") # De primera instacia observamos que el los valores se imprimieron de forma corrida sin realizar la operacion
 
-# Comentar por qué se realiza el casting de variables.
+# El casting se lleva acabo para realizar un cambio de dato ya que se pasan a numeros.
 numero1_float = float(numero1_cadena)
 numero2_float = float(numero2_cadena)
-resultado_float = numero1_float + numero2_float # Verificar qué es lo que realiza de esta manera y compáralo.
+resultado_float = numero1_float + numero2_float # Al hacer el casting facilita la operación por lo cual se llava acabo de manera óptima.
 print()
 print(" ****  Casting de varibles  ****")
 print(f"El resultado de {numero1_float} y {numero2_float} es: {resultado_float}")
